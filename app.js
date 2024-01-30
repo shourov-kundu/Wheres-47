fb3b6feaa51f63f569f2c052ebfb78d4ac9afc80
shourov-kundu <shourov.kundu@gmail.com>
2024-01-30 08:35:46 -0800

    new maps

--- app.js
+++ app.js
@@ -1,49 +1,60 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const mapButtons = document.getElementsByClassName('navbar__links');
-embed = document.getElementById('map');
 // Display mobile menu
 const mobileMenu = () => {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
-    var sideBar = embed.getElementsByClassName('item-count');
 
     console.log(document.getElementById('map').contentWindow.document.getElementsByClassName("navbar-dark"));
 };
 
-const loadMap = () => {
-    
-}
 for (let i = 0; i < mapButtons.length; i++) {
-    mapButtons[i].addEventListener('click', loadMap);
+    mapButtons[i].addEventListener('click', function(){
+
+        let x = document.getElementsByTagName('iframe');
+        if (x.length > 0){
+            if (x[0].id !== mapButtons[i].id){
+                x[0].remove();
+                var iframe = document.createElement('iframe');
+                iframe.id = mapButtons[i];
+                iframe.src = mapButtons[i] + ".html"; 
+                iframe.style.width = '1000px'; 
+                iframe.style.height = '600px'; 
+                
+                document.body.appendChild(iframe);
+                
+            }
+        }
+    });
 }
 
 menu.addEventListener('click', mobileMenu);
 
-document.getElementById('map').addEventListener('load', function(event){
-    var icons = document.getElementById('map').contentWindow.document.getElementsByClassName("leaflet-interactive");
+document.querySelector('iframe').addEventListener('load', function(event){
+    var icons = document.querySelector('iframe').contentWindow.document.getElementsByClassName("leaflet-interactive");
     var checkExist = setInterval(function() {
         if (icons.length > 100) {
             clearInterval(checkExist);
 
             // Press button to hide all elements (can't just alter tags because they are reset)
-            document.getElementById('map').contentWindow.document.getElementsByClassName("hide-select-button")[0].click();
+            document.querySelector('iframe').contentWindow.document.getElementsByClassName("hide-select-button")[0].click();
 
             // Open navigation menu and then select area button to show location names
-            document.getElementById('map').contentWindow.document.getElementsByClassName('card-header')[4].firstChild.click();
-            document.getElementById('map').contentWindow.document.querySelectorAll('img[alt="Area Icon"]')[0].click();
+            document.querySelector('iframe').contentWindow.document.getElementsByClassName('card-header')[4].firstChild.click();
+            document.querySelector('iframe').contentWindow.document.querySelectorAll('img[alt="Area Icon"]')[0].click();
 
             // Hide sidebar (all done with it by now)
-            document.getElementById('map').contentWindow.document.getElementsByClassName("navbar-dark")[0].style.visibility = 'hidden';
+            document.querySelector('iframe').contentWindow.document.getElementsByClassName("navbar-dark")[0].style.visibility = 'hidden';
 
             // Remove counts of items on each level
-            var counts = document.getElementById('map').contentWindow.document.getElementsByClassName('item-count');
+            var counts = document.querySelector('iframe').contentWindow.document.getElementsByClassName('item-count');
             while (counts.length) {
                 counts[0].remove();
             }
             
             // Make names unclickable (just for show)
-            var areas = document.getElementById('map').contentWindow.document.getElementsByClassName('area-icon');
+            var areas = document.querySelector('iframe').contentWindow.document.getElementsByClassName('area-icon');
             for (let i = 0; i < areas.length; i++) {
                 areas[i].classList.remove('leaflet-interactive');
               }
