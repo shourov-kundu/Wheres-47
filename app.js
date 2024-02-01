7a8ce87d010c71d0a2ac76c82e1184ad395603a5
shourov-kundu <shourov.kundu@gmail.com>
2024-02-01 00:30:19 -0800

    lotta stuff

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ for (let i = 0; i < mapButtons.length; i++) {
         if (x.length > 0){
             if (x[0].id !== mapButtons[i].id){
                 x[0].remove();
+                menu.classList.remove('is-active');
+                menuLinks.classList.remove('active');
                 document.getElementById("pin").style.visibility = 'hidden';
                 var iframe = document.createElement('iframe');
                 iframe.id = mapButtons[i].id;
@@ -23,7 +25,6 @@ for (let i = 0; i < mapButtons.length; i++) {
                 iframe.style.width = '50%'; 
                 iframe.style.height = '100%';  
                 iframe.setAttribute("scrolling", "no");
-                // document.body.appendChild(iframe, document.getElementById('home'));
                 document.getElementById('map-holder').insertBefore(iframe, document.getElementById('map-holder').firstChild);
                 document.querySelector('iframe').addEventListener('load', mapSetUp);
             }
@@ -33,8 +34,8 @@ for (let i = 0; i < mapButtons.length; i++) {
 
 menu.addEventListener('click', mobileMenu);
 const mapSetUp = () => {
-    var icons = document.querySelector('iframe').contentWindow.document.getElementsByClassName("leaflet-interactive");
     var checkExist = setInterval(function() {
+        var icons = document.querySelector('iframe').contentWindow.document.getElementsByClassName("leaflet-interactive");
         if (icons.length > 100) {
             clearInterval(checkExist);
 
@@ -61,13 +62,44 @@ const mapSetUp = () => {
                 areas[i].classList.remove('leaflet-interactive');
               }
         }
-     }, 25);
+     }, 10);
 }
 document.querySelector('iframe').addEventListener('load', mapSetUp);
 document.getElementById('guess-button').addEventListener('click', ()=>{
-    var imgElement = document.querySelector('iframe').contentWindow.document.querySelector('[style*="translate3d(158px, 255px, 0px)"]');
-    console.log(imgElement);
-    imgElement.style.display = 'block';
+    // var x = "//a[text()='The North']"
+    // var elements = document.querySelector('iframe').contentWindow.document.evaluate(x, document, null, XPathResult.ANY_TYPE, null);
+    // console.log(elements);
+    // let thisHeading = elements.iterateNext();
+    // while (thisHeading) {
+    //     console.log(thisHeading.textContent);
+    //     thisHeading = elements.iterateNext();
+    // }
+    var elements = document.querySelector('iframe').contentWindow.document.getElementsByClassName('area-icon');
+    console.log(elements.length)
+    for (let i = 0; i < elements.length; i++) {
+        if (elements[i].textContent.includes("The North")){
+            console.log(elements[i].style.transform);
+        }
+      }
 });
 
 //Use MATH to find player coordinates (extrapolate from icons)
+// Find absolute positions of two icons
+//Then find pin's absolute position (head) to find absolute difference in pixels
+
+
+//Paris, Bartoli's in security room
+// 376, 708 vs 376, 729 (fully zoomed in), 21 pixels 
+// space between elements remains constant after zooming in/out, resizing window, different browsers, even different resolutions
+// So, we can use distance between elements to find level of zoom. 
+// Once we know level of zoom, we can use calculate how far the distance in pixels actually represents, tell them in feet or meters or something
+
+// After
+// switch to correct floor
+// autozoom out as far as needed to show guess and actual location
+// then draw line between two (resizes with zoom?)
+
+// Show next button
+
+
+//Pictures: TO get mission-agnostic pictures use 
